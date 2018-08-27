3d51072df245c6be96bf789bc9f4181ecb090968
HunterFacts <hunterfacts@mail.ru>
2018-08-27 18:27:57 +0500

    New version 2.0

---
+++ CalculationPerMonth/Calc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationPerMonth
+{
+    public class Calc
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="price">Цена за подписку на 1 месяц.</param>
+        /// <param name="monthsAbsence">Месяцы исключение.</param>
+        /// <param name="monthssubscription">Месяцы подписки, выбранные пользователем.</param>
+        /// <returns>Возвращает цену за подписку с учетом скидки.</returns>
+        public string Calculation(int price, string[] monthsabsence, string[] monthssubscription)
+        {
+            bool check = false;
+            int discount = 0;
+            int result = 0;
+            foreach (string monthsub in monthssubscription)
+            {
+                check = !monthsabsence.Contains(monthsub);
+                if (check)
+                {
+                    result = result + price;
+                    discount++;
+                }               
+            }
+            return Returned(discount, result);
+           
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="discount">Количество месяцев для учета скидки</param>
+        /// <param name="result">Сумма оплаты без учёта скидки</param>
+        /// <returns>Возвращает цену за подписку с учетом скидки.</returns>
+        public string Returned (int discount, int result)
+        {
+            double res = result;
+            string dis = "0";
+            if (discount >= 12)
+            {
+                res = res * 0.90;
+                dis = "10";
+            }
+            else if (discount >= 6){
+                res = res * 0.95;
+                dis = "5";
+            }
+            string answer = res.ToString("0.00");
+            return $"Ваша цена за подписку представляет {answer}руб. (Скидка {dis}%)";
+        }
+    }
+}
---
+++ UnitTestProject1/CalcTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculationPerMonth;
+
+namespace CalculationPerMonthTest
+{
+    [TestClass]
+    public class CalcTest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void AllMonthsTest()
+        {
+            //September, October, November, December, January, February, March, April, May, June, July, August
+            string[] monthsub = new string[] { "September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August" };
+            string[] monthper = new string[] { "" };
+            int Price = 450;
+            Calc use = new Calc();
+            string answer = use.Calculation(Price, monthper, monthsub);
+            Console.WriteLine(answer);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void OneMonthTest()
+        {
+            //September, October, November, December, January, February, March, April, May, June, July, August
+            string[] monthsub = new string[] { "September" };
+            string[] monthper = new string[] { "" };
+            int Price = 450;
+            Calc use = new Calc();
+            string answer = use.Calculation(Price, monthper, monthsub);
+            Console.WriteLine(answer);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void ThreeMonthTest()
+        {
+            //September, October, November, December, January, February, March, April, May, June, July, August
+            string[] monthsub = new string[] { "September", "October", "December" };
+            string[] monthper = new string[] { "" };
+            int Price = 450;
+            Calc use = new Calc();
+            string answer = use.Calculation(Price, monthper, monthsub);
+            Console.WriteLine(answer);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void OneMonthAbuseTest()
+        {
+            //September, October, November, December, January, February, March, April, May, June, July, August
+            string[] monthsub = new string[] { "September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August" };
+            string[] monthper = new string[] { "March" };
+            int Price = 450;
+            Calc use = new Calc();
+            string answer = use.Calculation(Price, monthper, monthsub);
+            Console.WriteLine(answer);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void ThreeMonthAbuseTest()
+        {
+            //September, October, November, December, January, February, March, April, May, June, July, August
+            string[] monthsub = new string[] { "September", "October", "November", "December", "January", "February", "March", "April" };
+            string[] monthper = new string[] { "February", "October", "November" };
+            int Price = 450;
+            Calc use = new Calc();
+            string answer = use.Calculation(Price, monthper, monthsub);
+            Console.WriteLine(answer);
+        }
+
+    }
+}
